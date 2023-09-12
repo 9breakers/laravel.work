e9681f4782563756ca8df128fb7eb8cb88ebb290
9breakers <day38831@gmail.com>
2023-09-12 23:32:08 +0300

    Authorization improvements

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
@@ -39,5 +43,60 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+
+    protected $credentials = ['email','password'];
+
+
+    public function show(){
+        return view('auth.login');
+    }
+
+    public function create(LoginRequest $request){
+
+        $remember = $request->has('remember');
+
+        if (Auth::attempt($request->only($this->credentials), $remember)) {
+            if (Auth::check() && Auth::user()->is_admin == true) {
+                return redirect('/admin');
+
+            } else {
+                return redirect('/');
+            }
+        }
+        $user = User::where('email', $request->email)->first();
+        if ($user) {
+            return back()->withErrors([
+                'password' => 'Неправельний пароль'
+            ]);
+        } else {
+            return back()->withErrors([
+                'email' => 'Користувача з такою поштою не знайдено'
+            ]);
+        }
+
+    }
+
+    public function redirectGoogle(){
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callbackGoogle(){
+        $user=Socialite::driver('google')->user();
+        $this->regOrLogin($user);
+        return redirect()->route('home');
+    }
+
+    public function regOrLogin($data){
+        $user=User::where('email','=', $data->email)->first();
+        if(!$user){
+            return User::create([
+                'name'=>$data->name,
+                'email'=>$data->email,
+                'password'=>bcrypt(Str::random(10))
+            ]);
+        }
+        Auth::login($user);
+        return $user;
+    }
 
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -41,33 +43,23 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
+
+    public function show(){
+        return view('auth.registration');
     }
 
-    /**
-     * Create a new auth instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\Models\User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+    public function create(RegisterRequest $request){
+
+        $users= User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
         ]);
+
+        Auth::login($users);
+
+        return redirect('/');
     }
+
+
 }
